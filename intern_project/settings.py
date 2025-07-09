71f1e01b8ebd4f3c1ef934e353353a578930ae26
roman <damnfker@gmail.com>
2025-07-09 12:38:18 +0200

    поправки внесены

--- intern_project/settings.py
+++ intern_project/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
 ]
 
 MIDDLEWARE = [
@@ -82,8 +81,8 @@ DATABASES = {
         'NAME': os.environ.get("POSTGRES_DB"),
         'USER': os.environ.get("POSTGRES_USER"),
         'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
-        'HOST': "db",
-        'PORT': "5432",
+        'HOST': os.environ.get("POSTGRES_HOST"),
+        'PORT': os.environ.get("POSTGRES_PORT"),
     }
 }
 
